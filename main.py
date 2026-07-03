826c783e57df5e1fdb84ae47f1ac9bdff7291a5c
eben-vranken <vranken.eben@outlook.com>
2026-07-03 15:28:32 +0200

    feat: define entire IUPAC alphabet

--- main.py
+++ main.py
@@ -8,10 +8,50 @@ def main(fileName):
 
 def count_file(dna_string: str):
     nucleotide_counts = {
+        # Base codes
+        # Adenine
         "A": 0,
+        # Guanine
         "G": 0,
+        # Cytosine
         "C": 0,
+
+        # DNA Specific
+        # Thymine
         "T": 0,
+        
+        # RNA Specific
+        # Uracil
+        "U": 0,
+        
+        # Mix codes
+        # Adenine or Guanine
+        "R": 0,
+        # Cytosine or Thymine
+        "Y": 0,
+        # Guanine or Cytosine
+        "S": 0,
+        # Adenine or Thymine
+        "W": 0,
+        # Guanine or Thymine
+        "K": 0,
+        # Adenine or Cytosine
+        "M": 0,
+        # Cytosine or Guanine or Thymine
+        "B": 0,
+        # Adenine or Guanine or Thymine
+        "D": 0,
+        # Adenine or Cytosine or Thymine
+        "H": 0,
+        # Adenine or Cytosine or Guanine
+        "V": 0,
+        
+        # Any base
+        "N": 0,
+
+        # Gap codes
+        ".": 0,
+        "-": 0,
     }
 
     for c in dna_string.upper():
