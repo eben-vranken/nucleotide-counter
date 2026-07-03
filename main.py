f7c5925c4ca8664c514f5f6b44b5bc7cb175c99b
eben-vranken <vranken.eben@outlook.com>
2026-07-03 15:16:13 +0200

    feat: initial nucleotide counter logic

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 def main(fileName):
-    print("Initial commit!")
+    # Read file
+    with open(fileName, "r") as f:
+        dna_string: str = f.read()
+
+    # Parse and Count
+    count_file(dna_string)
+
+def count_file(dna_string: str):
+    nucleotide_counts = {
+        "A": 0,
+        "G": 0,
+        "C": 0,
+        "T": 0,
+    }
+
+    for c in dna_string.upper():
+        if c in nucleotide_counts:
+            nucleotide_counts[c] += 1
+
+    print(nucleotide_counts)
 
 if __name__ == "__main__":
-    main()
+    main("data/example_string.txt")
